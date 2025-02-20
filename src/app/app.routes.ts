2b42863365b5d392782e5340b4a5e97d0a40a49a
sadicGoblin <fgallardo@favric.cl>
2025-02-20 05:33:13 -0300

    burn-ticket component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { BurnTicketComponent } from './burn-ticket/burn-ticket.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent } // Ruta principal
+  { path: '', component: HomeComponent },
+  { path: 'ticket', component: BurnTicketComponent },
 ];
