2971b9d79041245aba100917eb39f0cadcb3e569
Jmarquez <jor.marquez82@gmail.com>
2025-03-17 18:30:44 -0300

    -3 zone horario + export

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -7,6 +7,8 @@ import { CommonModule } from '@angular/common';
 import { CodeTemplateComponent } from '../../utils/code-template/code-template.component';
 import * as XLSX from 'xlsx';
 import { saveAs } from 'file-saver';
+import moment from 'moment';
+import * as moment_ from 'moment-timezone';
 
 
 @Component({
@@ -15,21 +17,21 @@ import { saveAs } from 'file-saver';
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss'
 })
-export class AdminComponent implements AfterViewInit{
+export class AdminComponent implements AfterViewInit {
   displayedColumns: string[] = ['create', 'firstName', 'lastName', 'rut', 'phone', 'address', 'code', 'url'];
-  
+
   datos: any[] = [];
   dataSource!: MatTableDataSource<any>; // Define el tipo de datos de tus filas
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private firebaseService: FirebaseDatabaseService) {
-    
+
   }
 
   ngAfterViewInit(): void {
     this.getData();
-    
+
   }
 
 
@@ -38,7 +40,15 @@ export class AdminComponent implements AfterViewInit{
     console.log('get data!');
     this.firebaseService.getData('records').subscribe(data => {
       if (data) {
-        this.datos = Object.values(data); //  si la base de datos es un JSON
+        let datos: any[] = [];
+        datos = Object.values(data); //  si la base de datos es un JSON
+        const uniqueItems = datos.filter((item, index, self) =>
+          index === self.findIndex((t) => (
+            t.create === item.create // Ajusta esta condición según los atributos que consideres para verificar duplicados
+          ))
+        );
+
+        this.datos = uniqueItems;
         this.dataSource = new MatTableDataSource(this.datos.reverse());
         this.dataSource.paginator = this.paginator;
         console.log('data', this.datos);
@@ -47,6 +57,17 @@ export class AdminComponent implements AfterViewInit{
   }
 
   exportarAExcel(): void {
+
+    this.datos.forEach(item => {
+      // Convierte la fecha en formato ISO a un objeto Moment.js.
+      const originalDate = moment(item.create);
+
+      // Resta 3 horas
+      const adjustedDate = originalDate.subtract(3, 'hours');
+
+      // Asigna la fecha ajustada de nuevo en formato ISO
+      item.create = adjustedDate.toISOString();
+    });
     // Convertir los datos a un formato que Excel pueda entender
     const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.datos);
 
@@ -59,7 +80,7 @@ export class AdminComponent implements AfterViewInit{
 
     // Crear un Blob para el archivo
     const blob: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
-    
+
     // Usar file-saver para guardar el archivo
     saveAs(blob, 'datos.xlsx');
   }
